c0d6fbf0617e61e2961076020502f5a69bcd1663
Dania Salinas <dania.salinas@socialreality.com>
2018-06-06 21:08:57 -0700

    added example files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const App = () => {
+  return <div>Hallo</div>;
+}
+
+ReactDom.render(<App/>, document.querySelector('.container'));
+
